97fed71a9ec8f06005f69f0bf318cfcba48b056c
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2021-05-03 13:28:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,11 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # add cpu temperature 
 patch -p1 < package/my/add-patch_sun8i-h3-ths.patch
+#add chinese
+patch -p1 < package/my/luci_zh-cn.patch
 #cup mod
 sed -i 's/CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE/CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND/g' target/linux/sunxi/config-5.4
 sed -i 's/rootwait/rootwait g_ether.dev_addr=f8:dc:7a:5e:32:02 g_ether.host_addr=f8:dc:7a:5e:32:01/g' package/boot/uboot-sunxi/uEnv-default.txt
 #add usb_gadget
 cat package/my/sunxi-config >> target/linux/sunxi/config-5.4
-#add chinese
-for dir in $(realpath $(find package/lean -type d -name po)); do ln -s $dir/zh-cn $dir/zh_Hans; done
+
