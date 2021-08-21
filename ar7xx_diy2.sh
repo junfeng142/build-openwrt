2344dcb837bbf6d815a6b5c96a3364add414fb08
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2021-08-21 12:34:31 +0800

    Update ar7xx_diy2.sh

--- ar7xx_diy2.sh
+++ ar7xx_diy2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #rm node
-rm -rf feeds/packages/lang/node
+#rm -rf feeds/packages/lang/node
