6d2dca152732bfd845f0c7689cc3ca24e895497e
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2021-09-21 07:53:12 +0800

    Update diy2.sh

--- ar7xx/diy2.sh
+++ ar7xx/diy2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #add upx
 #mv ./upx ./staging_dir/host/bin && chmod +x staging_dir/host/bin/upx
