37f080febc00b1b9de086580dc5440de50e24dc4
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-03-02 12:23:41 +0800

    Update diy2.sh

--- mt762x/diy2.sh
+++ mt762x/diy2.sh
@@ -12,6 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# add upx
+mv ./upx ./staging_dir/host/bin
 # Change version for batman-adv
 # sed -i 's/2019.2/2017.3/g' feeds/routing/batman-adv/Makefile
 # sed -i 's/70c3f6a6cf88d2b25681a76768a52ed92d9fe992ba8e358368b6a8088757adc8/830edd297ff11b91623e9612172db3e2b2c1524e2f9b2ca1cb243e2e3f0ba4d4/g' feeds/routing/batman-adv/Makefile
