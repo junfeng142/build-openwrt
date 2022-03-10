331b3116d13751b754262610e7a4bc1ccb0e1870
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-03-10 16:44:55 +0800

    Update diy-official2.sh

--- orangepizero/diy-official2.sh
+++ orangepizero/diy-official2.sh
@@ -16,3 +16,5 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 mv ./upx ./staging_dir/host/bin && chmod +x staging_dir/host/bin/upx
 # helloworld
 sed -i '/PACKAGE_libustream/d' feeds/helloworld/luci-app-ssr-plus/Makefile
+# golang
+rm -rf ./feeds/packages/lang/golang && cp -r ./package/my/golang ./feeds/packages/lang
