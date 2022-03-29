f8e2321227dda37a0d73c367386f3a0c1e60282c
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-03-29 15:45:37 +0800

    Update and rename orangepizero/diy-official2.sh to sunxi/diy2.sh

--- sunxi/diy2.sh
+++ sunxi/diy2.sh
@@ -10,6 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# add cpu temperature（lede）
+#patch -p1 < package/my/add-patch_sun8i-h3-ths.patch
+#cup mod（lede）
+#sed -i 's/CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE/CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND/g' target/linux/sunxi/config-5.4
+#sed -i 's/rootwait/rootwait g_ether.dev_addr=f8:dc:7a:5e:32:02 g_ether.host_addr=f8:dc:7a:5e:32:01/g' package/boot/uboot-sunxi/uEnv-default.txt
+#add usb_gadget（lede）
+#cat package/my/sunxi-config >> target/linux/sunxi/config-5.4
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 # add upx
