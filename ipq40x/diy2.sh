0a38b9e5ed925f696f2a7c411551a8fc3c13688b
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-04-23 19:29:07 +0800

    Update diy2.sh

--- ipq40x/diy2.sh
+++ ipq40x/diy2.sh
@@ -12,10 +12,16 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
 # add upx
 mv ./upx ./staging_dir/host/bin && chmod +x ./staging_dir/host/bin/upx
+
 # batman-adv
 rm feeds/routing/batman-adv/patches/0004-Revert-batman-adv-genetlink-make-policy-common-to-fa.patch
 rm feeds/routing/batman-adv/patches/0037-batman-adv-allow-netlink-usage-in-unprivileged-conta.patch
+
+# golang
+rm -rf ./feeds/packages/lang/golang && cp -r ./package/own/golang ./feeds/packages/lang
+
 # helloworld
 #sed -i '/PACKAGE_libustream/d' feeds/helloworld/luci-app-ssr-plus/Makefile
