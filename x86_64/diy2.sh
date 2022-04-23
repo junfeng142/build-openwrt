6bf969d9d4ec5bc9c1448965fe9bef3e09876fbb
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-04-23 20:49:00 +0800

    Update diy2.sh

--- x86_64/diy2.sh
+++ x86_64/diy2.sh
@@ -12,5 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.100/g' package/base-files/files/bin/config_generate
+
 # add upx
 mv ./upx ./staging_dir/host/bin && chmod +x ./staging_dir/host/bin/upx
+
+# golang
+rm -rf ./feeds/packages/lang/golang && cp -r ./package/own/golang ./feeds/packages/lang
