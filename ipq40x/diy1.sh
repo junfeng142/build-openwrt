12b2478d5ec3fb2e2a921219a8319b0e47485c19
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-05-08 15:18:36 +0800

    Create diy1.sh

--- ipq40x/diy1.sh
+++ ipq40x/diy1.sh
@@ -17,6 +17,9 @@ git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/pass
 # clone own
 git clone https://github.com/junfeng142/packages.git package/own 
 
+# display cpu temp
+sed -i 's/thermal\/thermal_zone0\/temp/hwmon\/hwmon0\/temp1_input/g'  package/my/luci2-base/files/luci
+
 # Uncomment a feed source
 #sed -i '/network/s/^/#/g' package/my/default-settings/files/zzz-default-settings
 
