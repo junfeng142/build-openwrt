cf87b8820c8df6bd487f0550f3a46c0ab19fdd28
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-06-05 17:37:00 +0800

    Update diy2.sh

--- ipq40x/diy2.sh
+++ ipq40x/diy2.sh
@@ -20,8 +20,8 @@ mv ./upx ./staging_dir/host/bin && chmod +x ./staging_dir/host/bin/upx
 patch -p1 < package/own/patches/dhcp-dns.patch
 
 # batman-adv
-rm feeds/routing/batman-adv/patches/0004-Revert-batman-adv-genetlink-make-policy-common-to-fa.patch
-rm feeds/routing/batman-adv/patches/0037-batman-adv-allow-netlink-usage-in-unprivileged-conta.patch
+#rm feeds/routing/batman-adv/patches/0004-Revert-batman-adv-genetlink-make-policy-common-to-fa.patch
+#rm feeds/routing/batman-adv/patches/0037-batman-adv-allow-netlink-usage-in-unprivileged-conta.patch
 
 # golang
 rm -rf ./feeds/packages/lang/golang && cp -r ./package/own/golang ./feeds/packages/lang
