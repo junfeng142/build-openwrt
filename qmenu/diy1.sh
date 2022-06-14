e5b3ffe6c0ab9d81eabb85347115dcdd9380ba7a
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-06-14 16:48:16 +0800

    Update diy1.sh

--- qmenu/diy1.sh
+++ qmenu/diy1.sh
@@ -11,8 +11,8 @@
 #
 
 # clone passwall
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
 
 # clone helloworld
 git clone https://github.com/fw876/helloworld.git package/helloworld
