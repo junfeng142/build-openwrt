a92bb6ee8597dee499fb173ca33daac71475e497
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-06-23 11:41:26 +0800

    Update docker_armv7.sh

--- qmenu/docker/docker_armv7.sh
+++ qmenu/docker/docker_armv7.sh
@@ -15,15 +15,12 @@ mkdir -p "$TMPDIR"  && \
 mkdir -p "$OUTDIR"  && \
 gzip -dc openwrt-*-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
 cp -f patches/rc.local "$TMPDIR/etc/" && \
-cp -f patches/cpustat "$TMPDIR/usr/bin/" && chmod 755 "$TMPDIR/usr/bin/cpustat" && \
-cp -f patches/getcpu "$TMPDIR/bin/" && chmod 755 "$TMPDIR/bin/getcpu" && \
-cat patches/luci-admin-status-index-html.patch | (cd "$TMPDIR/usr/lib/lua/luci/view/admin_status/" && patch -p0) && \
 rm -f "$TMPDIR/etc/bench.log" && \
 echo "37 7 * * * /etc/coremark.sh" >> "$TMPDIR/etc/crontabs/root" && \
 rm -rf "$TMPDIR/lib/firmware/*" "$TMPDIR/lib/modules/*" && \
 (cd "$TMPDIR" && tar cf ../openwrt-default-rootfs-patched.tar .) && \
 rm -f DockerImg-OpenwrtArmv7-${TAG}.gz && \
-docker buildx build --no-cache --platform=linux/armv7 -o type=docker -t ${IMG_NAME}:${TAG} . && \
+docker buildx build --no-cache --platform=linux/arm/v7 -o type=docker -t ${IMG_NAME}:${TAG} . && \
 rm -f  openwrt-default-rootfs-patched.tar && \
 rm -rf "$TMPDIR" && \
 docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-${TAG}-${TAG}.gz
