b0eec2e807b08cda169296ac19d33929310599df
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-06-23 16:54:24 +0800

    Update docker_arm64.sh

--- qmenu/docker/docker_arm64.sh
+++ qmenu/docker/docker_arm64.sh
@@ -15,6 +15,7 @@ mkdir -p "$TMPDIR"  && \
 mkdir -p "$OUTDIR"  && \
 gzip -dc openwrt-armvirt-64-default-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
 cp -f rc.local "$TMPDIR/etc/" && \
+cp -f dhcp "$TMPDIR/etc/config/" && \
 rm -f "$TMPDIR/etc/bench.log" && \
 echo "37 7 * * * /etc/coremark.sh" >> "$TMPDIR/etc/crontabs/root" && \
 rm -rf "$TMPDIR/lib/firmware/*" "$TMPDIR/lib/modules/*" && \
