fdd3826da7174681ea346d230928b5d32091a273
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-06-26 20:50:41 +0800

    Update diy1.sh

--- ipq40x/diy1.sh
+++ ipq40x/diy1.sh
@@ -26,9 +26,3 @@ git clone https://github.com/junfeng142/packages.git package/own
 
 # display cpu temp
 sed -i 's/thermal\/thermal_zone0\/temp/hwmon\/hwmon1\/temp1_input/g'  package/my/luci2-base/files/luci
-
-# Uncomment a feed source
-#sed -i '/network/s/^/#/g' package/my/default-settings/files/zzz-default-settings
-
-# kernel patch
-#patch -p1 < ipq40x.patch
