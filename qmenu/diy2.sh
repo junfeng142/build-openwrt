9acb31fbe808565f3001fae9f5905b7e7c8ddaeb
junfeng142 <35558806+junfeng142@users.noreply.github.com>
2022-07-23 10:24:47 +0800

    Update diy2.sh

--- qmenu/diy2.sh
+++ qmenu/diy2.sh
@@ -16,6 +16,7 @@ sed -i 's/192.168.1.1/192.168.0.5/g' package/base-files/files/bin/config_generat
 # Modify
 sed -i 's/\"services\"/\"nas\"/g' package/aliyunwebd/openwrt/luci-app-aliyundrive-webdav/luasrc/controller/aliyundrive-webdav.lua
 sed -i 's/services/nas/g' package/aliyunwebd/openwrt/luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/aliyundrive-webdav_log.htm
+sed -i 's/services/nas/g' package/aliyunwebd/openwrt/luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/aliyundrive-webdav_qrcode.htm
 sed -i 's/services/nas/g' package/aliyunwebd/openwrt/luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/aliyundrive-webdav_status.htm
 
 sed -i 's/\"services\"/\"nas\"/g' package/aliyunfuse/openwrt/luci-app-aliyundrive-fuse/luasrc/controller/aliyundrive-fuse.lua
